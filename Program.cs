cc4bb17e7c4e5178ab4a25e062490d9ce5ba1509
pokharelshail <shreeshail.pokharel@bison.howard.edu>
2021-12-15 20:24:08 -0500

    Atm Project first commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace atm{
+
+    public class Program{
+
+        public static void Main(string[] args ){
+
+            
+
+
+            
+           var pinNames = new Dictionary<string, string>();
+           pinNames.Add("1111","John Doe");
+           pinNames.Add("2222","Evana Brr");
+           pinNames.Add("9999","/Shale");
+
+
+
+           string option ="";
+           string pin ="";
+
+           Console.WriteLine("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
+    
+
+           Console.WriteLine("         Welcome to $ CASH ATM\n");
+
+           Console.WriteLine("{1} Withdraw                 Deposit {2}");
+           Console.WriteLine("{3} Check Balance       Make Account {4}");
+
+          
+           Console.WriteLine("\n");
+           
+           while(true) {
+    
+                Console.Write("Choose Option: ");
+                option = Console.ReadLine();
+                if(option == "1" || option == "2"|| option =="3"|| option =="4"){
+                    break;
+                }
+            
+            }
+
+           Console.WriteLine("\n");
+
+           Console.Write("Enter Pin: ");
+           pin = Console.ReadLine();
+
+           Console.WriteLine($"\nWelcome {pinNames[pin]}!");
+           Console.WriteLine("\n");
+
+
+
+           Console.WriteLine("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
+
+        }
+    }
+}
